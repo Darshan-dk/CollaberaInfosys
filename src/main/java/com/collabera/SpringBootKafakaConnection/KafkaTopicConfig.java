41f3ec08dff5a783009ed1f2f5505830b385f636
darshankpoojary <darshankpoojari@gmail.com>
2021-07-20 20:52:04 +0530

    kafka Connection

---
+++ src/main/java/com/collabera/SpringBootKafakaConnection/KafkaTopicConfig.java
@@ -0,0 +1,12 @@
+package com.collabera.SpringBootKafakaConnection;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+	
+//	@Value
+//	private String kafkaAddress;
+
+}
---
+++ src/main/java/com/collabera/SpringBootKafakaConnection/SpringBootKafakaConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.SpringBootKafakaConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafakaConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafakaConnectionApplication.class, args);
+	}
+
+}
